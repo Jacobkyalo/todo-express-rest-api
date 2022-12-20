77c981dff5e79814d51b06074e28d8288b0f69c5
Jacob Kyalo <jacobkyalo43@gmail.com>
2022-12-20 14:10:31 +0300

    moved files to backend folder

---
+++ backend/controllers/todosController.js
@@ -0,0 +1,48 @@
+const Todo = require("../models/Todos");
+
+const getTodos = async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.status(200).json(todos);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const getTodo = async (req, res) => {
+    const todo = await Todo.findById(req.params.id).exec();
+    res.json(todo);
+};
+
+const createTodo = async (req, res) => {
+    const newTodo = new Todo({
+        title: req.body.title,
+        description: req.body.description,
+        completed: req.body.completed,
+    });
+
+    await newTodo.save();
+    res.status(201).json(newTodo);
+};
+
+const updateTodo = async (req, res) => {
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
+    res.status(200).json(updatedTodo);
+};
+
+const deleteTodo = async (req, res) => {
+    const todoToBeDeleted = Todo.findById(req.params.id);
+
+    await todoToBeDeleted.deleteOne();
+    res.json({ Message: "Todo deleted" });
+};
+
+module.exports = {
+    getTodos,
+    getTodo,
+    createTodo,
+    updateTodo,
+    deleteTodo,
+};
---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const routerRoutes = require("./routes/todos");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, () =>
+    console.log("Database connected successfully")
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/todos", routerRoutes);
+
+app.listen(port, () => console.log(`Server is running at port ${port}`));
