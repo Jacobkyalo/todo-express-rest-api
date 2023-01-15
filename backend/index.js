c2b7a9174e446eeefab0b1526f5434264a83175e
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-01-15 23:12:00 +0300

    used cors middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const routerRoutes = require("./routes/todos");
 
 const port = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ mongoose.connect(process.env.MONGODB_URI, () =>
     console.log("Database connected successfully")
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/todos", routerRoutes);
