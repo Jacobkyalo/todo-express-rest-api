a355a2c5e11aeda5957f2cfd738ea186740c0f4a
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-02-25 14:55:59 +0300

    updated find method

--- backend/controllers/todosController.js
+++ backend/controllers/todosController.js
@@ -2,7 +2,7 @@ const Todo = require("../models/Todos");
 
 const getTodos = async (req, res) => {
     try {
-        const todos = await Todo.find();
+        const todos = await Todo.find({});
         res.status(200).json(todos);
     } catch (error) {
         res.status(500).json({ message: error.message });
