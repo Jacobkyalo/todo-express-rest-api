bbd943da9344eb659dc4861ef5517bb47134433b
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-01 08:57:05 +0300

    interchanged components render

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,22 @@
-import React from "react";
 import Header from "../components/Header/Header";
+import { TodoForm } from "../components/TodoForm/TodoForm";
+import { TodoList } from "../components/TodoList/TodoList";
+import { TodoContextProvider } from "../contexts/TodoContext";
+import "./Home.scss";
 
 const Home = () => {
     return (
-        <div className="container">
-            <div className="container__wrapper">
-                <div className="container__content">
-                    <Header />
+        <TodoContextProvider>
+            <div className="container">
+                <div className="container__wrapper">
+                    <div className="container__content">
+                        <Header />
+                        <TodoForm />
+                        <TodoList />
+                    </div>
                 </div>
             </div>
-        </div>
+        </TodoContextProvider>
     );
 };
 
