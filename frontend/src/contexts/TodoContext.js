6075093f46c93a7f08a5387314d74a44624d54d2
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-01 09:02:31 +0300

    todo context update

--- frontend/src/contexts/TodoContext.js
+++ frontend/src/contexts/TodoContext.js
@@ -1,16 +1,17 @@
-import { createContext } from "react";
-
-const initialStates = {
-    loading: false,
-    error: false,
-    data: [],
-};
+import { createContext, useState } from "react";
 
 export const TodoContext = createContext(null);
 
 export const TodoContextProvider = ({ children }) => {
+    const [todos, setTodos] = useState([]);
+
     return (
-        <TodoContext.Provider value={initialStates}>
+        <TodoContext.Provider
+            value={{
+                todos,
+                setTodos,
+            }}
+        >
             {children}
         </TodoContext.Provider>
     );
