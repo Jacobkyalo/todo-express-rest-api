3e8f16a04e952d1e4a380a45bb806e4b7d9c228d
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-01 09:01:11 +0300

    handled fetch and added some inline styles

--- frontend/src/components/TodoList/TodoList.jsx
+++ frontend/src/components/TodoList/TodoList.jsx
@@ -1,16 +1,39 @@
-import useFetchHook from "../../hooks/useFetchHook";
+import { useContext, useEffect } from "react";
+import Axios from "axios";
+import { TodoContext } from "../../contexts/TodoContext";
 import { Todo } from "../Todo/Todo";
 
 export const TodoList = () => {
-    const { state } = useFetchHook("http://localhost:5000/api/todos");
+    const { todos, setTodos } = useContext(TodoContext);
+
+    const fetchTodos = async () => {
+        const res = await Axios.get("http://localhost:5000/api/todos");
+        const data = await res.data;
+        setTodos(data);
+        console.log(res.data);
+    };
+
+    useEffect(() => {
+        fetchTodos();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <>
-            <ul>
-                {state?.data.map((todo) => (
+            <ul style={{ listStyle: "none" }}>
+                {todos?.map((todo) => (
                     <Todo key={todo._id} todo={todo} />
                 ))}
             </ul>
+            <p
+                style={{
+                    marginTop: "1.5rem",
+                    fontSize: "1.4rem",
+                    color: "#fff",
+                }}
+            >
+                You have {todos?.length} todos 🥰
+            </p>
         </>
     );
 };
